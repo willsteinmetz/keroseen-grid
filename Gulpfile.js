3bb99765ff889811c68ebc0bc9be46b51a6ac3c8
Will Steinmetz <willsteinmetz@me.com>
2016-03-10 19:59:33 -0500

    Updating gulp package versions

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,24 +8,22 @@
 var gulp       = require('gulp'),
   sass         = require('gulp-ruby-sass'),
   autoprefixer = require('gulp-autoprefixer'),
-  minifycss    = require('gulp-minify-css'),
-//  uglify       = require('gulp-uglify'),
+  cleanCss     = require('gulp-clean-css'),
   rename       = require('gulp-rename'),
   clean        = require('gulp-clean'),
-  header       = require('gulp-header');//,
-//  sourcemaps   = require('gulp-sourcemaps');
+  header       = require('gulp-header');
 
 // styles task
-gulp.task('styles', function () {
+gulp.task('styles', ['clean'], function () {
   return gulp.src('src/sass/keroseen-grid.scss')
     .pipe(sass({
       style: 'expanded',
       loadPath: [__dirname + '/src/sass']
     }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('dist'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
+    .pipe(cleanCss())
     .pipe(header('/**\n\
  * @author Will Steinmetz\n\
  * @license MIT\n\
@@ -37,13 +35,14 @@ gulp.task('styles', function () {
 
 // clean up task
 gulp.task('clean', function () {
-  return gulp.src(['dist', 'src/css/**/*.css'], {read: false})
+  return gulp.src(['dist'], {read: false})
     .pipe(clean());
 });
 
 // default task
 gulp.task('default', ['clean'], function () {
   gulp.start('styles');
+  gulp.start('watch');
 });
 
 // watch task
